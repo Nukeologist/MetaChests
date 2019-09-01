4b731bc019e930f520f7dd39190bd3f5dbb4ffe3
Nukeologist <guilhermebignelli@terra.com.br>
2019-09-01 17:48:25 -0300

    update license on all files.

--- src/main/java/nukeologist/metachests/MetaChestTileEntity.java
+++ src/main/java/nukeologist/metachests/MetaChestTileEntity.java
@@ -1,3 +1,23 @@
+/*
+ *
+ *  *     Copyright © 2019 Nukeologist
+ *  *     This file is part of MetaChests.
+ *  *
+ *  *     MetaChests is free software: you can redistribute it and/or modify
+ *  *     it under the terms of the GNU General Public License as published by
+ *  *     the Free Software Foundation, either version 3 of the License, or
+ *  *     (at your option) any later version.
+ *  *
+ *  *     MetaChests is distributed in the hope that it will be useful,
+ *  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  *     GNU General Public License for more details.
+ *  *
+ *  *     You should have received a copy of the GNU General Public License
+ *  *     along with MetaChests.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package nukeologist.metachests;
 
 import net.minecraft.entity.player.PlayerEntity;
@@ -19,7 +39,6 @@ import net.minecraftforge.items.ItemStackHandler;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.Comparator;
 
 public class MetaChestTileEntity extends TileEntity implements INamedContainerProvider {
 
